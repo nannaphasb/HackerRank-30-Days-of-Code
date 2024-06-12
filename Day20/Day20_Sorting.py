3777da6b3640cb48aa13373f8fce03957065516a
nannaphasb <nannaphas.bc@gmail.com>
2024-06-12 13:02:43 +0700

    Add Day 20 - Sorting

---
+++ Day20/Day20_Sorting.py
@@ -0,0 +1,19 @@
+n = int(input().strip())
+a = list(map(int, input().rstrip().split()))
+
+swaps = 0
+
+for i in range(n):
+    for j in range(n - 1):
+        if a[j] > a[j + 1]:
+            tmp = a[j]
+            a[j] = a[j + 1]
+            a[j + 1] = tmp
+            swaps += 1
+
+    if swaps == 0:
+        break
+
+print("Array is sorted in " + str(swaps) + " swaps.")
+print("First Element: " + str(a[0]))
+print("Last Element: " + str(a[-1]))
